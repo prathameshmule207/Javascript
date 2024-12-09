d79c05102d59ec348fdad2ed270747620252638a
Prathamesh Mule <prathameshmule207@gmail.com>
2024-12-09 11:22:40 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,44 @@
+/*let score = "prathamesh"
+
+let ValueOfIntNumber = Number(score);
+
+
+console.log(typeof(score));
+
+console.log(typeof ValueOfIntNumber);
+
+console.log(ValueOfIntNumber);  */
+
+
+
+
+// 33 =>33
+// "123abc"=>Nan  not a number
+//true=> 1
+//false=>0
+
+
+
+/*let  IsLoggedIn = 1
+
+let BooleanIsLOggedIn = Boolean(IsLoggedIn);
+
+console.log(BooleanIsLOggedIn);*/
+
+
+//1=>true
+//0=> false
+//"" => false
+//"hishe"=>true
+
+
+let Somenumber = 1;
+
+let StringNumber = String(Somenumber);
+
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
+
+
+
