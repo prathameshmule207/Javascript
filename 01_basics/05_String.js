73e4ae7aac8ecb58af54fa6898d870f9d8879810
Prathamesh Mule <prathameshmule207@gmail.com>
2024-12-10 10:47:22 +0000

    new

---
+++ 01_basics/05_String.js
@@ -0,0 +1,60 @@
+const name = "prathamesh";
+const repoCount = 50;
+
+//console.log(name + repoCount + " on Github");
+// console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`)
+
+
+const GameName = new String("Pratham Gaming");
+
+// console.log(GameName[2]);
+
+// console.log(GameName.__proto__);
+
+// console.log(GameName.length);
+
+// console.log(GameName.toUpperCase());
+
+// console.log(GameName.charAt(2));
+
+// console.log(GameName.indexOf('t'));
+
+// const newGameName = GameName.slice(1,3)
+// console.log(newGameName);
+
+// const NewGame = GameName.substring(1,4);
+// console.log(NewGame);
+
+
+
+
+let mailid = "        prathameshmule%207%@gmail.com      "
+console.log(mailid);
+
+console.log(mailid.trim());
+
+console.log(mailid.replaceAll('%','-'));
+
+console.log(mailid.includes('prathamesh'));
+console.log(mailid.includes('ravi'));
+
+console.log(mailid.split('%'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
